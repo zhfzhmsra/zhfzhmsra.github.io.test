4a77deebc978f0f2032e95b9fafc6abc966f9037
zhfzhmsra <zhfzhmsra@gmail.com>
2018-05-16 15:24:50 +0800

    s

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Osaka, Japan",
+    34.6198813,
+    135.490357
   ],
   [
     "London, UK",
@@ -18,5 +18,20 @@ var addressPoints = [
     "Los Angeles, CA",
     34.0543942,
     -118.2439408
+  ],
+  [
+    "Zurich, Switzerland",
+    47.3723957,
+    8.5423216
+  ],
+  [
+    "Beijing, China",
+    40.190632,
+    116.412144
+  ],
+  [
+    "Shenzhen, China",
+    22.5445697,
+    114.0545346
   ]
 ];
